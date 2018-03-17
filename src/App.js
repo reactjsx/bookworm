72232f1b2b9e2a3bf5cd91019bae5a92029975e8
Trung Tran <chun.techexplained@gmail.com>
2018-03-17 16:37:39 +0900

    React Router setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
-const App =() => (
-  <div className="App">
-    <header className="App-header">
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-);
+const App =() => <div>
+  <Route path="/" exact component={HomePage} />
+  <Route path="/login" exact component={LoginPage} />
+</div>;
 
 export default App;
